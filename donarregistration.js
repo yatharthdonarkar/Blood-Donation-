d9db571c66c784d993dfba480d3d6cf975aa742d
yatharthdonarkar <yatharthdonarkar13@gmail.com>
2025-02-28 13:22:09 +0530

    Uploaded all files
    We have Upgraded our website with image , more information and registration forms also we create data base for all the forms we have created

---
+++ donarregistration.js
@@ -0,0 +1,6 @@
+document.getElementById('registration-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Handle form submission logic here
+    alert('Thank you for registering as a donor!');
+  });
+  
